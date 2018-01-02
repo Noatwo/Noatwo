506d6336429ec6a5fdaed29154c3be5f5a64829b
Noatwo <1065478834@qq.com>
2018-01-02 17:06:45 +0800

    asd

---
+++ src/Xrz/Xrz.java
@@ -0,0 +1,19 @@
+package Xrz;
+
+import java.util.Scanner;
+
+public class Xrz {
+	public static void shuchu(int a){
+	Scanner ac=new Scanner(System.in);
+	System.out.println("请输入一个数");
+	int b=ac.nextInt();
+	for(int i=0;i<b;i++){
+		System.out.println(i+1);
+		
+		
+		
+	}
+		
+	
+	}
+}
